503d70c4f3cfc677a2435d6efa3cd0c713a1747a
fathamfasyaa <orowodol1808@gmail.com>
2025-10-23 16:33:33 +0700

    remove template coreui (berat)

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -42,6 +42,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        // Redirect ke login setelah logout
+        return redirect()->route('login');
     }
 }
